dbbb45bb3f078a5360e52612138a86cc72a914c0
Nashiruddin <nashiruddin.abubakar@gmail.com>
2017-04-13 11:25:07 +0700

    Instal react-native-vector-icons to android(manually)

--- android/app/src/main/java/com/proofn/MainApplication.java
+++ android/app/src/main/java/com/proofn/MainApplication.java
@@ -12,6 +12,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.oblador.vectoricons.VectorIconsPackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -30,7 +31,8 @@ public class MainApplication extends Application implements ReactApplication {
           new MainReactPackage(),
             new VectorIconsPackage(),
             new RNSimpleAlertDialogPackage(),
-            new ReactNativeI18n()
+            new ReactNativeI18n(),
+           new VectorIconsPackage()
       );
     }
   };
